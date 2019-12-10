d02d53ac09e7c3d3eccdc6b82a021cf77584e5e1
Miroslav Jackanin <miroslav.jackanin@akademiasovy.sk>
2019-12-10 15:40:35 +0100

    cleaned up customCSS.css and home.php

--- _partials/header.php
+++ _partials/header.php
@@ -21,8 +21,8 @@ if (!isset($_SESSION['email'])) {
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="./_assets/css/normalize.css">
     <link rel="stylesheet" href="./_assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
-   
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+
     <!-- DELETE ECHO TIME IN THE END !!!  ... REFRESHING CSS -->
     <link rel="stylesheet" href="_assets/css/customCSS.css ?timestamp=<?php echo time() ?>">
 
@@ -30,30 +30,30 @@ if (!isset($_SESSION['email'])) {
     <title>DOable</title>
 </head>
 
-<body>
+<body style="padding-top: 200px">
     <header>
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark justify-content-around">
-            <div>
+        <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark justify-content-around">
             <a class="navbar-brand" href="./home.php" style="font-size: 50px"><span class="text-success">DO</span>able</a>
-            </div>
             <div class="taskButton">
-                <a class="btn btn-outline-success btn-lg nav-link" id="nav-tasks" href="home.php">TASKS</a>
+                <a class="btn btn-outline-success btn-lg nav-link" id="nav-tasks" href="today.php" style="border-radius: 10px 0 0 10px">DAILY</a>
+                <a class="btn btn-outline-success btn-lg nav-link" id="nav-tasks" href="home.php" style="border-radius: 0 0 0 0">TASKS</a>
                 <a class="btn btn-success btn-lg nav-link" id="nav-add-task"><span class="fas fa-plus"></span></a>
             </div>
 
             <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
-        
-                <div class="signedin">
-                    <?php
+            <div class="navbar-collapse collapse justify-content-between" id="navbarColor01" >
+                <ul class="navbar-nav" style="vertical-align: middle">
+                    <li class="signedin">
+                        <?php
                         if (!empty($_SESSION['email'])) {
                             echo '<a href="userAccount.php">' . $_SESSION['email']. '</a>';
                             echo '<span style="margin: 0 20px 0 20px"><a href="./_inc/logout.php">LOG OUT</a></span>';
                         }
-                    ?>
-                </div>
-            
+                        ?>
+                    </li>
+                </ul>
+            </div>
         </nav>
     </header>
--- render.php
+++ render.php
@@ -34,7 +34,7 @@
             .'</div>'
         .'</div>';
         } else {
-            return 
+            return
             '<div class="card text-white bg-success mb-3" style = "max-width: 30rem;">'
             .'<div class="row justify-content-between align-items-center card-header" style = "width: 100%; max-width:489px; margin:0;">'
                 .'<div>'.'<h5>'.$title.'</h5>'.'</div>'.'<br>'
@@ -43,14 +43,14 @@
                     .'<form action="_inc/delete_item.php" method="get">'
                         .'<input type="hidden" name="id" value='.$id.'>'
                         .'<input onclick="return checkdelete()" class="btn btn-outline-danger delete-form-btn" style="margin-left: 5px;" type="submit" name="delete-item" value="Delete">'
-                    .'</form>' 
-                    
+                    .'</form>'
+
                     .'<form action="edit_page.php" id="edit-form-btn" method="get">'
                     .'<input type="hidden" name="edit_id" value='.$id.'>'
-                    
+
                     .'<input  class="btn btn-outline-warning" style="margin-left: 5px;" type="submit" name="edit_item" value="Edit">'
-                    .'</form>'    
-                          
+                    .'</form>'
+
                     .'<button class="btn btn-outline-primary" style="margin-left: 5px;" data-toggle="collapse" data-target="#demo'.$id.'">'.'+'.'</button>'
                 .'</div>'
             .'</div>'
